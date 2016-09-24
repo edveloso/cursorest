6be4868074e8a0661de3dc8ba9ce5a9e398df4b9
edveloso <edveloso@gmail.com>
2016-09-24 01:45:29 -0300

    send all

---
+++ restapp/src/br/com/oportunidades/Todo.java
@@ -0,0 +1,48 @@
+package br.com.oportunidades;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Produces(MediaType.APPLICATION_JSON)
+public class Todo {
+
+	private String nome;
+	private int idade;
+
+	
+	public Todo() {
+	}
+
+	public Todo(String nome) {
+		this.nome = nome;
+	}
+
+	public Todo(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
